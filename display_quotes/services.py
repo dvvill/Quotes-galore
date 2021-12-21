a9f084c48391d115e9ce5551b0e153a9a61d7ffe
dvvill <91037458+dvvill@users.noreply.github.com>
2021-12-21 17:59:25 -0500

    create second app for admin user/superuser

--- display_quotes/services.py
+++ display_quotes/services.py
@@ -9,20 +9,25 @@ api_token = "ddXJz0o6BPz2lWEtGOzsrQeF"
 headers = {'content-type': 'application/json',
 	   'X-TheySaidSo-Api-Secret': format(api_token)}
 
-# response = requests.get(url, headers=headers)
-# quotes = response.json()['contents']['quotes'][0]
-# print(quotes)
+response = requests.get(url, headers=headers)
+quotes = response.json()['contents']['quotes'][0]
+print(quotes)
 
+
+# def get_quotes():
+#     response = requests.get(url, headers=headers)
+#     quotes = response.json()['contents'][0]
+#     print(quotes)
 
 # Function to send the request and return the response/account info in a python dictionary 
-def get_quotes():    
-    url = 'https://quotes.rest/qod?category=management'
-    response = requests.get(url, headers=headers)
-    quotes = response.json()['contents']['quotes'][0]
-    quote_list = []
-    for i in range(len(quotes['quotes'])):
-        quote_list.append(quotes['quotes'][i])
-    return quote_list
+# def get_quotes():    
+#     url = 'https://quotes.rest/qod?category=management'
+#     response = requests.get(url, headers=headers)
+#     quotes = response.json()['contents']['quotes'][0]
+#     quote_list = []
+#     for i in range(len(quotes['quotes'])):
+#         quote_list.append(quotes['quotes'][i])
+#     return quote_list
 
 
 
